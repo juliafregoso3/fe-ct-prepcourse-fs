480dadb8012ede2958bdf5d4cd89beec4fbd463d
JuliaFregosoUrrutia <juliafregoso3@gmail.com>
2023-02-06 21:53:07 -0600

    Completé tareas JSI y JSII

--- M03 JavaScript Fundamentos/Ejercicio 04.js
+++ M03 JavaScript Fundamentos/Ejercicio 04.js
@@ -6,31 +6,50 @@
 function elevarAlCuadrado(num) {
    // Retorna el valor de "num" elevado al cuadrado.
    // Tu código:
+   /*var cuadrado = Math.pow(num, 2);
+   return cuadrado;
+   */
+  return num ** 2;
 }
 
 function elevarAlCubo(num) {
    // Retorna el valor de "num" elevado al cubo.
    // Tu código:
+   var cubo = Math.pow(num, 3);
+   return cubo;
+   /* return num ** 3; */
 }
 
 function elevar(num, exponent) {
    // Retorna el valor de "num" elevado al exponente "exponent".
    // Tu código:
+   var elevado = Math.pow(num, exponent);
+   return elevado;
+   // return num ** exponent;
 }
 
 function redondearNumero(num) {
    // Redondea "num" al entero más próximo y retórnalo.
    // Tu código:
+   var redondeado = Math.round(num);
+   return redondeado;
+   // return Math.round(num);
 }
 
 function redondearHaciaArriba(num) {
    // Redondea "num" hacia arriba y retórnalo.
    // Tu código:
+   var redondeadoArriba = Math.ceil(num);
+   return redondeadoArriba;
+   //return Math.ceil(num);
 }
 
 function numeroRandom() {
    // Genera un número al azar entre 0 y 1 y retórnalo.
    // Tu código:
+   var numeroRandom = Math.random();
+   return numeroRandom;
+   // return Math.random();
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
---
+++ M04 JavaScript Bucles/arrays.js
@@ -0,0 +1,6 @@
+var listaDeCompras = [];
+listaDeCompras[3] = 'Tomates';
+console.log(listaDeCompras);
+   
+
+
